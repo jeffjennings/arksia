7800fafc5f0daa8f450e065f8e2a1094c21bab3e
Jeff Jennings <jjennings1519@gmail.com>
2023-06-11 23:27:00 -0400

    radial pipeline: bulk runner syntax

--- bulk_pipeline_run.py
+++ bulk_pipeline_run.py
@@ -3,11 +3,9 @@
 import os
 import json
 
-def main(gen_par_f='default_gen_pars.json', source_par_f='default_source_pars.json'):
-    gen_pars = json.load(open(gen_par_f, 'r'))
-    source_pars = json.load(open(source_par_f, 'r'))
-
+def main(gen_par_f='./default_gen_pars.json', source_par_f='./default_source_pars.json'):
     disk_names = []
+    source_pars = json.load(open(source_par_f, 'r'))
     for ii in source_pars:
         disk_names.append(ii)
 
@@ -15,8 +13,8 @@ def main(gen_par_f='default_gen_pars.json', source_par_f='default_source_pars.js
     for ii, jj in enumerate(disk_names):
         print("\nPipeline call {} of {} - disk {}".format(ii, len(disk_names) - 1, jj))
 
-        os.system('python radial_profile_pipeline.py -d {} -b {} -s {}'.format(
-        jj, gen_pars, source_pars))
+        os.system('python radial_pipeline.py -d {} -b {} -s {}'.format(
+        jj, gen_par_f, source_par_f))
 
 if __name__ == "__main__":
     main()
