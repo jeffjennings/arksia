b473a893641bbfa92b36aaeffa300945a5e0b666
Jeff Jennings <jjennings1519@gmail.com>
2023-06-20 23:57:38 -0400

    radial pipeline: generalize concatenate_vis

--- input_output.py
+++ input_output.py
@@ -8,17 +8,16 @@ import astropy.io.fits as pyfits
 from frank.utilities import generic_dht, get_fit_stat_uncer
 from frank.io import load_sol, load_uvtable
 
-def concatenate_vis(in1, in2, out=None):
+def concatenate_vis(in_paths, out_path):
     """
-    Concatenate the visibilities from two ascii files; save as one output .npz.
+    Concatenate the visibilities from multiple files; save as one output .npz file.
 
     Parameters
     ----------
-    in1, in2 : string
+    in_paths : list of string
         Filenames of the input visibility tables
-    out : string, optional
-        Filename of the output table. If not supplied, the name will be set as
-        vis_combined.npz
+    out_path : string
+        Filename of the output table
 
     Returns
     -------
@@ -26,24 +25,22 @@ def concatenate_vis(in1, in2, out=None):
         concatenated dataset: u-coordinates, v-coordinates, visibility amplitudes 
         (Re(V) + Im(V) * 1j), weights
     """
-    u1, v1, re1, im1, weights1, _ = np.genfromtxt(in1).T
-    u2, v2, re2, im2, weights2, _ = np.genfromtxt(in2).T
+    u, v, re, im, weights = [], [], [], [], []
+    for ii in in_paths:
+        try: 
+            ut, vt, ret, imt, weightst, _ = np.genfromtxt(ii).T
+            u.extend(ut)
+            v.extend(vt)
+            re.extend(ret)
+            im.extend(imt)
+            weights.extend(weightst)
+        except OSError:
+            pass
 
-    u = np.concatenate((u1, u2))
-    v = np.concatenate((v1, v2))
-    re = np.concatenate((re1, re2))
-    im = np.concatenate((im1, im2))
-    vis = re + im * 1j
-    weights = np.concatenate((weights1, weights2))
+    u, v, weights = np.array(u), np.array(v), np.array(weights)
+    vis = np.add(re, np.multiply(im, 1j))
 
-    if out is None:
-        parts1 = in1.split('/')
-        parts2 = in2.split('/')
-        base1 = os.path.splitext(parts1[4])[0]
-        base2 = os.path.splitext(parts2[4])[0]
-        out = "{}/vis_combined.npz".format(os.path.join(*parts1[:4]))
-
-    np.savez(out,
+    np.savez(out_path,
              u=u, v=v, V=vis, weights=weights,
              units={'u': 'lambda', 'v': 'lambda',
                     'V': 'Jy', 'weights': 'Jy^-2'})
