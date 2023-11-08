08567b23448d0a2fbe69828bfc9180dfb416a1b6
Jeff Jennings <jjennings1519@gmail.com>
2023-11-08 13:53:41 -0500

    parametric_forms: reduce imports

--- arksia/parametric_fitter.py
+++ arksia/parametric_fitter.py
@@ -1,15 +1,12 @@
 """This module contains a class to fit parametric forms to a radial brightness signal 
 (written by Jeff Jennings)."""
 
-import random
-import numpy as np
-import matplotlib.pyplot as plt
 from tqdm.auto import tqdm
 import jax.numpy as jnp
 import jax
 import optax 
 
-from arksia.parametric_forms import *
+from arksia import parametric_forms
 
 class ParametricFit():
     """
