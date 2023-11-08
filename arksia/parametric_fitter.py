a635ed4db3520146e8a8bd6f1d4c462cbf9230c1
Jeff Jennings <jjennings1519@gmail.com>
2023-11-08 01:29:31 -0500

    start of ParametricFit class

--- arksia/parametric_fitter.py
+++ arksia/parametric_fitter.py
@@ -11,3 +11,16 @@ import optax
 
 from arksia.parametric_forms import *
 
+class ParametricFit():
+    """
+    # TODO
+    """
+
+    def __init__(self, truth, model, learn_rate=1e-3, niter=10000):
+
+
+        self._model = model 
+
+        self._learn_rate = learn_rate
+        self._niter = niter
+
