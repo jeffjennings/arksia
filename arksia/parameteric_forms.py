9d73c13cffde60bb58daee32a76ce2e217b8f000
Jeff Jennings <jjennings1519@gmail.com>
2023-11-07 23:05:33 -0500

    parametric_forms: add asymmetric gauss

--- arksia/parameteric_forms.py
+++ arksia/parameteric_forms.py
@@ -16,3 +16,19 @@ def gauss(r: jnp.ndarray, Rc: jnp.float32, sigma: jnp.float32):
 
     return jnp.exp(-(r - Rc) ** 2 / (2 * sigma ** 2))
 
+
+def asym_gauss(params: optax.Params, r: jnp.ndarray):
+    """
+    Assymetric (piecewise) Gaussian function f(r) of the form:
+
+        .. math::
+
+            \Sigma(r) = \exp\left[ \dfrac{-(r-R_{c})^{2}}{2\sigma_{\rm{in}}^{2}} \right] \mathrm{for} r < R_{c}, 
+            \Sigma(r) = \exp\left[ \dfrac{-(r-R_{c})^{2}}{2\sigma_{\rm{out}}^{2}} \right] \mathrm{for} r \geq R_{c}
+    """      
+
+    return jnp.piecewise(r, [r < params['Rc'], r >= params['Rc']], 
+                        [lambda r : gauss(r, params['Rc'], params['sigma_in']), 
+                         lambda r : gauss(r, params['Rc'], params['sigma_out'])]
+                         )
+
