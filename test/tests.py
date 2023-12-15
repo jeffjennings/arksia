059b8e33843c9c9671cfec1b100438f810c26e61
Jeff Jennings <jjennings1519@gmail.com>
2023-12-15 15:11:36 -0500

    start of tests

---
+++ test/tests.py
@@ -0,0 +1,7 @@
+"""This module runs tests to confirm the code is working correctly."""
+
+import numpy as np 
+
+def load_test_data():
+    """Load dataset used for testing"""
+    dset = np.load('test/test1.npz')
