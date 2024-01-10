49e4512526bb648eefdd99232d69c3556a7fca8f
Jeff Jennings <jjennings1519@gmail.com>
2024-01-09 19:20:41 -0500

    tests: simplify inputs

--- test/tests.py
+++ test/tests.py
@@ -5,23 +5,18 @@ import json
 import numpy as np 
 
 from arksia import pipeline
-    
-def load_test_data():
-    """Load dataset used for testing"""
-    dset = np.load('test/test1.npz')
 
-# def test_model_setup():
+def save_custom_gen_pars(gen_pars):
+    """Save an altered generic parameters file"""
 
-def _run_pipeline(make_figs=False, multifit=False):
-    
     tmp_dir = '/tmp/arksia/tests'
     os.makedirs(tmp_dir, exist_ok=True)
+    gen_pars['base']['output_dir'] = tmp_dir
 
-    # Save the new parameter file
-    params = []
-    param_file = os.path.join(tmp_dir, 'gen.json')
-    with open(param_file, 'w') as f:
-        json.dump(params, f)
+    gen_pars_file = os.path.join(tmp_dir, 'gen_pars.json')
+    with open(gen_pars_file, 'w') as f:
+        json.dump(gen_pars, f)
 
-    # Call the pipeline to perform the fit
-    pipeline.main(['-p', param_file])
+    return gen_pars_file
+
+
