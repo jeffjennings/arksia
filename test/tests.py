c88218fa73fa2f52bc6388e4c023800d5d9df7d4
Jeff Jennings <jjennings1519@gmail.com>
2024-01-09 19:21:40 -0500

    tests: frank vertical fit

--- test/tests.py
+++ test/tests.py
@@ -90,3 +90,17 @@ def test_pipeline_frank_multifit():
     _run_pipeline(gen_pars_file)
 
 
+def test_pipeline_frank_vertical_fit():
+    """Run the pipeline to perform a frank fit with vertical inference"""
+
+    gen_pars = pipeline.load_default_parameters()
+
+    gen_pars = update_frank_pars(gen_pars)
+
+    gen_pars['frank']['scale_height'] = 1e-1
+
+    gen_pars_file = save_custom_gen_pars(gen_pars)
+
+    _run_pipeline(gen_pars_file)
+
+
