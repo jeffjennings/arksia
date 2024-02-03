dbbc8a227a0d4ab7a1c1f1e08050e5c50f8167d7
Jeff Jennings <jjennings1519@gmail.com>
2024-02-02 19:26:22 -0500

    add analysis module

---
+++ arksia/analysis.py
@@ -0,0 +1,9 @@
+"""This module contains functions for some basic analysis and associated plotting of pipeline results 
+(written by Jeff Jennings)."""
+
+import json 
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt 
+from scipy.interpolate import interp1d
+
