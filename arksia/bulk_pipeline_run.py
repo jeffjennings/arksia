503efbe11e896b8004679f1ac793a32575688dca
Jeff Jennings <jjennings1519@gmail.com>
2024-06-27 00:58:37 -0400

    bulk_pipeline_run: option to reproduce best frank fits

--- arksia/bulk_pipeline_run.py
+++ arksia/bulk_pipeline_run.py
@@ -7,6 +7,7 @@ import json
 def survey_pipeline(gen_par_f='./pars_gen.json', 
          source_par_f='./pars_source.json',
          phys_par_f='./summary_disc_parameters.csv',
+        reproduce_best_frank_fits=False,
          ):
     """
     Run the arksia pipeline across multiple survey sources.
@@ -18,7 +19,9 @@ def survey_pipeline(gen_par_f='./pars_gen.json',
     source_par_f : string, default='pars_source.json'
         Path to the parameter file with custom values for each source         
     phys_par_f : string, default='pars_gen.json'
-        Path to the physical parameters file    
+        Path to the physical parameters file
+    reproduce_best_frank_fits : bool, default=False
+        Whether to reproduce frank fits with the best fit values in `pars_source.json`
     """    
     
     disk_names = []
@@ -32,6 +35,9 @@ def survey_pipeline(gen_par_f='./pars_gen.json',
         print(f"\nPipeline call {ii + 1} of {len(disk_names)} - disk {jj}")
 
         gen_pars['base']['input_dir'] = f"./{jj}"
+        if reproduce_best_frank_fits is True:
+            gen_pars["base"]["reproduce_best_frank"] = True
+
         gen_pars_current = os.path.join(os.path.dirname(gen_par_f), 'pars_gen_temp.json')
         with open(gen_pars_current, 'w') as f:
             json.dump(gen_pars, f)
